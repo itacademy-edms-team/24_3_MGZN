70b2f5ba3934ddc006ca5bc91962f1dc28a11324
cybersj5 <163285309+cybersj5@users.noreply.github.com>
2025-04-14 15:05:22 +0700

    Добавил ProductRepository

---
+++ source/backend/InShop.WebAPI/InShopDataLayer/Abstractions/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using InShopDataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InShopDataLayer.Abstractions
+{
+    public interface  IProductRepository
+    {
+        Task<List<Product>> GetProducts();
+        void DeleteProduct(int id);
+        void CreateProduct(Product product);
+        void UpdateProducts(int id, string name,
+                                 string? description,
+                                 decimal price,
+                                 bool availability,
+                                 int categoryID,
+                                 int stockQuantity,
+                                 string? imageURL);
+    }
+}
